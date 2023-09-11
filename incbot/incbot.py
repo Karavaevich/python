9b4be5bf525323a4692953d129485a16ca6803bd
Vitaly Karavaev <karavaevich@gmail.com>
2023-09-12 02:09:43 +0300

    changes for vm

--- incbot/incbot.py
+++ incbot/incbot.py
@@ -61,7 +61,7 @@ def webhook():
         json_string = flask.request.get_data().decode('utf-8')
         update = telebot.types.Update.de_json(json_string)
         bot.process_new_updates([update])
-        return ''
+        return f'{json_string}'
     else:
         flask.abort(403)
 
