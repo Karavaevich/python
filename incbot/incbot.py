7e9df7869480babaa04983e56b2abb83f505c907
Vitaly Karavaev <karavaevich@gmail.com>
2023-09-15 20:46:42 +0300

    list_of_incs to dict_of_incs

--- incbot/incbot.py
+++ incbot/incbot.py
@@ -107,11 +107,11 @@ def get_user_text(message):
                 for i in range(2, list_of_words_from_mes.__len__()):
                     des += str(list_of_words_from_mes[i]) + ' '
                 if list_of_words_from_mes[list_of_words_from_mes.__len__() - 1].lower() == 'ок':
-                    update_inc(inc_num=int(list_of_words_from_mes[1]) - 1, text=des.removesuffix('ок '), end=get_now())
-                    bot.send_message(message.chat.id, print_inc(get_inc(inc_num=int(list_of_words_from_mes[1]) - 1)))
+                    update_inc(inc_num=int(list_of_words_from_mes[1]), text=des.removesuffix('ок '), end=get_now())
+                    bot.send_message(message.chat.id, print_inc(get_inc(inc_num=int(list_of_words_from_mes[1]))))
                 else:
-                    update_inc(inc_num=int(list_of_words_from_mes[1]) - 1, text=des)
-                    bot.send_message(message.chat.id, print_inc(get_inc(inc_num=int(list_of_words_from_mes[1]) - 1)))
+                    update_inc(inc_num=int(list_of_words_from_mes[1]), text=des)
+                    bot.send_message(message.chat.id, print_inc(get_inc(inc_num=int(list_of_words_from_mes[1]))))
     elif list_of_words_from_mes[0].lower() == 'всеинц':
         bot.send_message(message.chat.id, str(print_dict_of_incs(dict_of_incs)))
     elif list_of_words_from_mes[0].lower() == 'всеинцудалить':
