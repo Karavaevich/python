2a189215eba8e68cffaab681af82e57a640c0c98
Vitaly Karavaev <karavaevich@gmail.com>
2023-09-17 21:05:32 +0300

    delete inc

--- incbot/incbot.py
+++ incbot/incbot.py
@@ -105,7 +105,7 @@ def get_user_text(message):
             if list_of_words_from_mes.__len__() == 2:
                 bot.send_message(message.chat.id, print_inc(get_inc(inc_num=int(list_of_words_from_mes[1]))))
             if list_of_words_from_mes.__len__() > 2:
-                if list_of_words_from_mes[3].lower() == 'удалить':
+                if list_of_words_from_mes[2].lower() == 'удалить':
                     bot.send_message(message.chat.id, print_inc(dict_of_incs.pop(int(list_of_words_from_mes[1]))))
                 des = ''
                 for i in range(2, list_of_words_from_mes.__len__()):
