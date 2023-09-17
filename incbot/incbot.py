b3a8e2b0e5720838b4199e6f4801b5a04e0ad5d7
Vitaly Karavaev <karavaevich@gmail.com>
2023-09-17 22:32:30 +0300

    tks

--- incbot/incbot.py
+++ incbot/incbot.py
@@ -110,9 +110,9 @@ def get_user_text(message):
                         bot.send_message(message.chat.id,
                                          'удалено событие:\n' + print_inc(dict_of_incs.pop(int(
                                              list_of_words_from_mes[1]))))
-                    if (list_of_words_from_mes.__len__() > 3) & (list_of_words_from_mes[2].lower() == 'ткс') & (list_of_words_from_mes[3].isnumeric()):
-                        update_inc(inc_num=int(list_of_words_from_mes[1]), tks_num=list_of_words_from_mes[3])
-                        bot.send_message(message.chat.id, print_inc(get_inc(inc_num=int(list_of_words_from_mes[1]))))
+                    # if (list_of_words_from_mes.__len__() > 3) & (list_of_words_from_mes[2].lower() == 'ткс') & (list_of_words_from_mes[3].isnumeric()):
+                    #     update_inc(inc_num=int(list_of_words_from_mes[1]), tks_num=list_of_words_from_mes[3])
+                    #     bot.send_message(message.chat.id, print_inc(get_inc(inc_num=int(list_of_words_from_mes[1]))))
                     des = ''
                     for i in range(2, list_of_words_from_mes.__len__()):
                         des += str(list_of_words_from_mes[i]) + ' '
