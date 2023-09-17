2dcd7e6cba55aad21b3f7281b89b7581227a824b
Vitaly Karavaev <karavaevich@gmail.com>
2023-09-17 22:37:36 +0300

    tks

--- incbot/incbot.py
+++ incbot/incbot.py
@@ -106,9 +106,9 @@ def get_user_text(message):
                 if list_of_words_from_mes.__len__() == 2:
                     bot.send_message(message.chat.id, print_inc(get_inc(inc_num=int(list_of_words_from_mes[1]))))
                 if list_of_words_from_mes.__len__() > 2:
-                    if (list_of_words_from_mes.__len__() > 3) & (list_of_words_from_mes[2].lower() == 'ткс') & (list_of_words_from_mes[3].isnumeric()):
-                        update_inc(inc_num=int(list_of_words_from_mes[1]), tks_num=list_of_words_from_mes[3])
-                        bot.send_message(message.chat.id, print_inc(get_inc(inc_num=int(list_of_words_from_mes[1]))))
+                    # if (list_of_words_from_mes.__len__() > 3) & (list_of_words_from_mes[2].lower() == 'ткс') & (list_of_words_from_mes[3].isnumeric()):
+                    #     update_inc(inc_num=int(list_of_words_from_mes[1]), tks_num=list_of_words_from_mes[3])
+                    #     bot.send_message(message.chat.id, print_inc(get_inc(inc_num=int(list_of_words_from_mes[1]))))
                     if list_of_words_from_mes[2].lower() == 'удалить':
                         bot.send_message(message.chat.id,
                                          'удалено событие:\n' + print_inc(dict_of_incs.pop(int(
